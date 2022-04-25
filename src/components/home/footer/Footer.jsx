4ae11482a00d0f2363e8c97990fe278a7abaceb6
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-25 13:43:02 +0200

    feat(components) : ajout de contenu dans Footer

--- src/components/home/footer/Footer.jsx
+++ src/components/home/footer/Footer.jsx
@@ -4,7 +4,29 @@ import './footer.scss'
 const Footer = () => {
   return (
     <div className='footer'>
+      <div className="footer__container">
+        <div className="footer__top">
+          <div className="footer__top-container">
+            <span>
+              Abonnez-vous à la newsletter MYSA pour recevoir du contenu exclusif, des offres et des réductions !
+            </span>
+            <div className="footer__input">
+              <input type="text" />
+              <span>Enregistrez-moi</span>
+            </div>
+            <div className="footer__social-networks">
 
+            </div>
+          </div>
+
+        </div>
+        <div className="footer__bottom">
+          <div className="footer__bottom-text">
+            <span className='footer__all-right'>© 2022 MYSA · ALL RIGHTS RESERVED</span>
+            <span className='footer__made-by'>ECOMMERCE WEBSITE MADE BY KEVIN</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
