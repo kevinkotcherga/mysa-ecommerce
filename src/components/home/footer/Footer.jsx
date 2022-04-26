2460828d697776f067e58a8b885a4d9ac14b586e
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-26 12:09:41 +0200

    feat(components) : ajout de contenu dans Footer

--- src/components/home/footer/Footer.jsx
+++ src/components/home/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './footer.scss'
+import { AiOutlineTwitter, AiOutlineInstagram } from 'react-icons/ai';
+import { BsFacebook } from 'react-icons/bs';
 
 const Footer = () => {
   return (
@@ -7,18 +9,46 @@ const Footer = () => {
       <div className="footer__container">
         <div className="footer__top">
           <div className="footer__top-container">
-            <span>
-              Abonnez-vous à la newsletter MYSA pour recevoir du contenu exclusif, des offres et des réductions !
+            <div className="footer__top-left">
+              <span className='footer__message'>
+              Abonnez-vous à la newsletter MYSA pour recevoir du contenu exclusif, des offres<br/> et des réductions !
             </span>
             <div className="footer__input">
-              <input type="text" />
-              <span>Enregistrez-moi</span>
+              <input type="text" placeholder='EMAIL'/>
+              <span>ENVOYER</span>
             </div>
             <div className="footer__social-networks">
-
+              <AiOutlineTwitter className='footer__logo' color={"#ffffff"} />
+              <BsFacebook className='footer__logo' color={"#ffffff"} />
+              <AiOutlineInstagram className='footer__logo' color={"#ffffff"} />
+            </div>
+            </div>
+            <div className="footer__top-right">
+              <div className="footer__explore">
+              <span>EXPLORE</span>
+              <ul>
+                <li>Shop</li>
+                <li>About</li>
+                <li>Trade</li>
+              </ul>
+              </div>
+              <div className="footer__customer-service">
+                <span>CUSTOMER SERVICE</span>
+                <ul>
+                  <li>Frequently Asked</li>
+                  <li>Contact Us</li>
+                </ul>
+              </div>
+              <div className="footer__legal">
+                <span>LEGAL</span>
+                <ul>
+                  <li>Delivery & Returns</li>
+                  <li>Terms & Conditions</li>
+                  <li>Privacy Policy</li>
+                </ul>
+              </div>
             </div>
           </div>
-
         </div>
         <div className="footer__bottom">
           <div className="footer__bottom-text">
